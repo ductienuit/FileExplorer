8552cc21475d15953f7b26a4c7ef092e78407212
Duc Tien <ductien.uit@gmail.com>
2018-08-28 09:14:46 +0700

    initialize

---
+++ app/src/main/java/com/app/intership/fileexplorer/ExplorerActivity.kt
@@ -0,0 +1,12 @@
+package com.app.intership.fileexplorer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExplorerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explorer)
+    }
+}
